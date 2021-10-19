dfb39424ae715582db45df928a272928b33a8873
EddieH <ehajek@outlook.com>
2021-10-19 15:32:29 -0400

    install MERN applications, update server.js file similar to lesson

--- server/server.js
+++ server/server.js
@@ -1,10 +1,29 @@
 const express = require('express');
+const {ApolloServer} = require('apollo-server-express');
 const path = require('path');
-const db = require('./config/connection');
-const routes = require('./routes');
 
-const app = express();
+const {typeDefs, resolvers} = require('./schemas');
+const {authMiddleware} = require('./utils/auth');
+const db = require('./config/connection');
+
 const PORT = process.env.PORT || 3001;
+const app = express();
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
+// Apollo 3.0 Testing
+// server.start().then(res => {
+//   server.applyMiddleware({ app });
+//   app.listen({ port: `${PORT}` }, () =>
+//     console.log(`API server running on port ${PORT}!`),
+//     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`)
+//   )
+// });
+
+server.applyMiddleware({ app });
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -14,8 +33,14 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
