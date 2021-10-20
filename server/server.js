ca2e3cd717694fd1560d3d74021b049a55b1ae62
EddieH <ehajek@outlook.com>
2021-10-20 11:39:52 -0400

    working with support on apollo issue

--- server/server.js
+++ server/server.js
@@ -22,7 +22,12 @@ const server = new ApolloServer({
 //     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`)
 //   )
 // });
-server.applyMiddleware({ app });
+async function startServer() {
+  await server.start();
+  server.applyMiddleware({ app });
+}
+
+startServer();
 
 
 app.use(express.urlencoded({ extended: true }));
